a85e37bc31c9efcd3e2ea82ba927a4b73c469c67
FxmousKid <36068362+FxmousKid@users.noreply.github.com>
2025-04-27 23:12:38 +0200

    feat(back): added sawgger setup with dark mode

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,12 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
 		new FastifyAdapter(),
 	);
+
+	const config = new DocumentBuilder()
+		.setTitle('API Documentation')
+		.setDescription('Description of the APIs')
+		.setVersion('0.1')
+		.addTag('api')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document, {
+		  customCss: readFileSync('./swagger/SwaggerDark.css', 'utf8'),
+	});
+
+	// Enable CORS
+	app.enableCors({
+		origin: '*', // Allow all origins
+		methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+		allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+	});
+	
 	await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
