fcc51e3264b92a5b7606680fae453ce3cbe90f7f
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-05-04 11:14:00 +0200

    chore: pushing useless scripts for reminders later on

--- app/vault/scripts/approle_01.sh
+++ app/vault/scripts/approle_01.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 vault auth enable approle
-cat > "agent-policy.hcl" << EOF
-path "secret/data/dbinfo" {
-  capabilities = ["read"]
-}
-EOF
 
-vault policy write agent-policy agent-policy.hcl
 vault write auth/approle/role/vault-agent-role policies="agent-policy"
+
 vault read auth/approle/role/vault-agent-role/role-id
 vault write -f auth/approle/role/vault-agent-role/secret-id
